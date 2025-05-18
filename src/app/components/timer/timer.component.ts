41e869eb2f6965031fd804eab3dc022b41a37626
Gabriel <rodriguesgasoares@gmail.com>
2025-05-17 22:07:40 -0300

    feat: criando a pagina do timer de forma estatica

---
+++ src/app/components/timer/timer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-timer',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './timer.component.html',
+  styleUrl: './timer.component.scss'
+})
+export class TimerComponent {
+
+}
